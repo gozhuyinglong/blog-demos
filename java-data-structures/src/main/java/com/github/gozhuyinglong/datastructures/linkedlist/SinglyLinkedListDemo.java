0283e7344e42039cf2f2ee0d15cdd7004981284b
zhuyinglong <15065421873@163.com>
2020-11-20 18:22:25 +0800

    update singly linked list

--- java-data-structures/src/main/java/com/github/gozhuyinglong/datastructures/linkedlist/SinglyLinkedListDemo.java
+++ java-data-structures/src/main/java/com/github/gozhuyinglong/datastructures/linkedlist/SinglyLinkedListDemo.java
@@ -10,16 +10,34 @@ public class SinglyLinkedListDemo {
 
     public static void main(String[] args) {
         Node node1 = new Node(1, "张三");
-        Node node2 = new Node(9, "李四");
-        Node node3 = new Node(6, "王五");
-        Node node4 = new Node(3, "王五");
+        Node node2 = new Node(3, "李四");
+        Node node3 = new Node(7, "王五");
+        Node node4 = new Node(5, "赵六");
 
         SinglyLinkedList singlyLinkedList = new SinglyLinkedList();
+        System.out.println("-----------添加节点（尾部）");
         singlyLinkedList.add(node1);
         singlyLinkedList.add(node2);
         singlyLinkedList.add(node3);
         singlyLinkedList.add(node4);
+        singlyLinkedList.print();
 
+        System.out.println("-----------获取某个节点");
+        Node node = singlyLinkedList.get(3);
+        System.out.println(node);
+
+        singlyLinkedList.remove(node3);
+        System.out.println("-----------移除节点");
+        singlyLinkedList.print();
+
+        System.out.println("-----------修改节点");
+        node4.setValue("赵六2");
+        singlyLinkedList.update(node4);
+        singlyLinkedList.print();
+
+        System.out.println("-----------按顺序添加节点");
+        Node node5 = new Node(4, "王朝");
+        singlyLinkedList.addOfOrder(node5);
         singlyLinkedList.print();
 
     }
@@ -47,6 +65,46 @@ public class SinglyLinkedListDemo {
         }
 
         /**
+         * 按顺序添加节点
+         *
+         * @param node
+         */
+        public void addOfOrder(Node node) {
+            Node temp = head;
+            while (true) {
+                if (temp.next == null) {
+                    temp.next = node;
+                    break;
+                } else if(temp.next.key > node.getKey()){
+                    node.next = temp.next;
+                    temp.next = node;
+                    break;
+                }
+                temp = temp.next;
+            }
+        }
+
+        /**
+         * 获取某个节点
+         *
+         * @param key
+         * @return
+         */
+        public Node get(int key) {
+            if (head.next == null) {
+                return null;
+            }
+            Node temp = head.next;
+            while (temp != null) {
+                if (temp.key == key) {
+                    return temp;
+                }
+                temp = temp.next;
+            }
+            return null;
+        }
+
+        /**
          * 移除一个节点
          *
          * @param node
@@ -57,7 +115,7 @@ public class SinglyLinkedListDemo {
                 if (temp.next == null) {
                     break;
                 }
-                if (temp.no == node.no) {
+                if (temp.next.key == node.key) {
                     temp.next = temp.next.next;
                     break;
                 }
@@ -76,8 +134,8 @@ public class SinglyLinkedListDemo {
                 if (temp.next == null) {
                     break;
                 }
-                if (temp.no == node.no) {
-                    temp.name = node.name;
+                if (temp.key == node.key) {
+                    temp.value = node.value;
                     break;
                 }
                 temp = temp.next;
@@ -99,44 +157,36 @@ public class SinglyLinkedListDemo {
 
 
     private static class Node {
-        private int no;
-        private String name;
+        private final int key;
+        private String value;
         private Node next;
 
-        public Node(int no, String name) {
-            this.no = no;
-            this.name = name;
+        public Node(int key, String value) {
+            this.key = key;
+            this.value = value;
         }
 
-        public int getNo() {
-            return no;
+        public int getKey() {
+            return key;
         }
 
-        public void setNo(int no) {
-            this.no = no;
+        public String getValue() {
+            return value;
         }
 
-        public String getName() {
-            return name;
-        }
-
-        public void setName(String name) {
-            this.name = name;
+        public void setValue(String value) {
+            this.value = value;
         }
 
         public Node getNext() {
             return next;
         }
 
-        public void setNext(Node next) {
-            this.next = next;
-        }
-
         @Override
         public String toString() {
             return "Node{" +
-                    "no=" + no +
-                    ", name='" + name + '\'' +
+                    "key=" + key +
+                    ", value='" + value + '\'' +
                     '}';
         }
     }
