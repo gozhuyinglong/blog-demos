1d4e478ed64cd24ca70142fb429c8c2865b419a5
gozhuyinglong <gozhuyinglong@163.com>
2020-12-28 18:41:08 +0800

    B树

--- java-data-structures/src/main/java/io/github/gozhuyinglong/datastructures/tree/BTreeDemo.java
+++ java-data-structures/src/main/java/io/github/gozhuyinglong/datastructures/tree/BTreeDemo.java
@@ -1,240 +1,282 @@
 package io.github.gozhuyinglong.datastructures.tree;
 
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * B树
  *
  * @author ZhuYinglong
- * @date 2020/12/22 0022
+ * @date 2020/12/28 0028
  */
 public class BTreeDemo {
 
-
     public static void main(String[] args) {
 
-
+        int[] item = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17};
+        BTree tree = new BTree();
+        for (int i : item) {
+            tree.add(new Entry(i, "-->" + i));
+        }
+        Entry entry = tree.searchEntry(16);
+        System.out.println(entry.getValue());
     }
 
+    /**
+     * B树类
+     */
     private static class BTree {
 
-        private Node root;
+        private static final int M = 5; // B树的阶
+        private Node root; // 树根
 
         public Node getRoot() {
             return root;
         }
 
         /**
-         * 添加一个键
+         * 根据键搜索数据记录
          *
-         * @param entry
+         * @param key
+         * @return
          */
-        public void add(Entry<Integer, String> entry) {
-            if (entry == null) {
-                return;
-            }
-
-            if (root == null) {
-                Node node = new Node();
-                node.add(entry);
-                return;
-            }
-            add(entry, root);
+        public Entry searchEntry(int key) {
+            return searchEntry(root, key);
         }
 
         /**
-         * 添加一个键，递归插入
+         * 根据键搜索数据记录 - 递归
+         *
+         * @param node
+         * @param key
+         * @return
+         */
+        private Entry searchEntry(Node node, int key) {
+            if (node == null) {
+                return null;
+            }
+            // 使用二分查找法定位下标
+            int index = Collections.binarySearch(node.getEntrys(), new Entry(key, null));
+            if (index >= 0) {
+                return node.getEntrys().get(index);
+            } else {
+                if (node.getChildNode().size() == 0) {
+                    return null;
+                }
+                return searchEntry(node.getChildNode().get(-index - 1), key);
+            }
+        }
+
+        /**
+         * 添加元素
          *
          * @param entry
-         * @param node
          */
-        private void add(Entry<Integer, String> entry, Node node) {
+        public void add(Entry entry) {
+            // root为空，直接创建
+            if (root == null) {
+                Node node = new Node();
+                node.add(entry);
+                root = node;
+                return;
+            }
+            add(root, entry);
+        }
 
-            // 当前节点为叶子节点，开始插入
-            if (node.getChildSize() == 0) {
+        /**
+         * 添加元素 - 递归
+         *
+         * @param node
+         * @param entry
+         */
+        private void add(Node node, Entry entry) {
 
-                // 当前节点已满，分裂该节点
-                if (node.entrys.length == node.getSize()) {
-                    // 未达到最大分支，则直接分裂
-                    // 若已达到最大分支，则...
+            // 当前节点为叶子节点
+            if (node.getChildNode().size() == 0) {
+
+                // 如果当前节点元素未满，直接添加元素
+                if (node.getEntrys().size() < M - 1) {
+                    node.add(entry);
                     return;
                 }
-
-                // 当前节点未满，则直接插入
-                node.add(entry);
+                // 如果当前节点元素已满，则分裂当前节点
+                node.getEntrys().add(entry);
+                split(node);
                 return;
             }
 
             // 当前节点为内部节点，继续往下调用（新插入的节点，只能是叶子节点）
-            for (int i = 0; i < node.entrys.length; i++) {
-                if (node.entrys[i] == null || node.entrys[i].key > entry.key) {
-                    add(entry, node.getChildNode()[i]);
-                    break;
-                }
-
-                if (node.entrys.length == i + 1) {
-                    add(entry, node.getChildNode()[i + 1]);
-                }
+            // 使用二分查找法找到要插入的分支
+            int index = Collections.binarySearch(node.getEntrys(), entry);
+            if (index < 0) {
+                add(node.getChildNode().get(-index - 1), entry);
             }
-
         }
 
         /**
-         * 使用顺序查找法，找到应该插入的位置
+         * 分离当前节点
          *
-         * @param entrys
-         * @param target
-         * @return
+         * @param node
          */
-        private int sequenceSearchIndex(Entry<Integer, String>[] entrys, Entry<Integer, String> target) {
-            int i = 0;
-            for (; i < entrys.length; i++) {
-                // 添加到数组尾部
-                if (entrys[i] == null || entrys[i].key > target.key) {
-                    break;
+        private void split(Node node) {
+            int mid = node.getEntrys().size() / 2;
+            // 分隔值
+            Entry separateEntry = node.getEntrys().get(mid);
+            // 分离后的左节点
+            Node leftNode = new Node();
+            leftNode.getEntrys().addAll(node.getEntrys().subList(0, mid));
+            // 分离后的右节点
+            Node rightNode = new Node();
+            rightNode.getEntrys().addAll(node.getEntrys().subList(mid + 1, node.getEntrys().size()));
+            // 分离子节点
+            if (node.getChildNode().size() > 0) {
+                List<Node> leftChildNode = node.getChildNode().subList(0, mid + 1);
+                for (Node temp : leftChildNode) {
+                    temp.setParentNode(leftNode);
+                }
+                leftNode.getChildNode().addAll(leftChildNode);
+
+                List<Node> rightChildNode = node.getChildNode().subList(mid + 1, node.getEntrys().size() + 1);
+                for (Node temp : rightChildNode) {
+                    temp.setParentNode(rightNode);
+                }
+                rightNode.getChildNode().addAll(rightChildNode);
+            }
+
+            // 当前节点为根节点
+            if (node.parentNode == null) {
+                Node newRoot = new Node();
+                newRoot.add(separateEntry);
+                root = newRoot;
+                leftNode.parentNode = root;
+                rightNode.parentNode = root;
+                root.addChild(leftNode).addChild(rightNode);
+            } else {
+                node.parentNode.add(separateEntry);
+                leftNode.parentNode = node.parentNode;
+                rightNode.parentNode = node.parentNode;
+                node.parentNode.addChild(leftNode).addChild(rightNode);
+                node.parentNode.getChildNode().remove(node);
+                // 若其父节点溢出，继续分裂
+                if (node.parentNode.getEntrys().size() > M - 1) {
+                    split(node.parentNode);
                 }
             }
-            return i;
         }
+
+        /**
+         * 删除元素
+         *
+         * @param key
+         */
+        public void remove(int key) {
+            remove(root, key);
+        }
+
+        /**
+         * 删除元素 - 递归
+         *
+         * @param node
+         * @param key
+         */
+        private void remove(Node node, int key) {
+
+        }
+
     }
 
-    private static class Node {
+    /**
+     * B树的节点类
+     */
+    private static class Node implements Comparable<Node> {
 
-        public static final int M = 3; // B树的阶
-
-        private final Entry<Integer, String>[] entrys; // 当前节点的键值对
+        private final List<Entry> entrys; // 当前节点的键值对
+        private final List<Node> childNode; // 子节点，使用数组存储子节点
         private Node parentNode; // 父节点
-        private Node[] childNode; // 子节点，使用数组存储子节点
 
-        public Node getParentNode() {
-            return parentNode;
+        public Node() {
+            entrys = new ArrayList<>();
+            childNode = new ArrayList<>();
+        }
+
+        public List<Entry> getEntrys() {
+            return entrys;
+        }
+
+        public List<Node> getChildNode() {
+            return childNode;
         }
 
         public void setParentNode(Node parentNode) {
             this.parentNode = parentNode;
         }
 
-        public Node[] getChildNode() {
-            return childNode;
+        public Node getParentNode() {
+            return parentNode;
         }
 
-        public Node() {
-            entrys = new Entry[M - 1];
-            childNode = new Node[M];
+        public Node add(Entry entry) {
+            entrys.add(entry);
+            Collections.sort(entrys);
+            return this;
+        }
+
+        public Node addChild(Node node) {
+            childNode.add(node);
+            Collections.sort(childNode);
+            return this;
         }
 
         /**
-         * 获取当前节点键的大小
+         * 按照键值中第一个key进行排序
          *
+         * @param o
          * @return
          */
-        public int getSize() {
-            int i = 0;
-            for (; i < entrys.length; i++) {
-                if (entrys[i] == null) {
-                    break;
-                }
-            }
-            return i;
+        @Override
+        public int compareTo(Node o) {
+            return Integer.compare(entrys.get(0).getKey(), o.getEntrys().get(0).getKey());
         }
-
-        /**
-         * 添加一个键，并保持顺序
-         *
-         * @param entry
-         */
-        public void add(Entry<Integer, String> entry) {
-            if (entry == null) {
-                return;
-            }
-            for (int i = 0; i < entrys.length; i++) {
-                // 添加到数组尾部
-                if (entrys[i] == null) {
-                    entrys[i] = entry;
-                    break;
-                }
-                // 添加到数组中间
-                if (entrys[i].key > entry.key) {
-                    System.arraycopy(entrys, i, entrys, i + 1, M - i - 1);
-                    entrys[i] = entry;
-                    break;
-                }
-            }
-        }
-
-        /**
-         * 获取子节点数量
-         *
-         * @return
-         */
-        public int getChildSize() {
-            int i = 0;
-            for (; i < childNode.length; i++) {
-                if (childNode[i] == null) {
-                    break;
-                }
-            }
-            return i;
-        }
-
-        /**
-         * 添加一个子节点，并保持顺序
-         *
-         * @param node
-         */
-        public void addChild(Node node) {
-            if (node == null) {
-                return;
-            }
-            for (int i = 0; i < entrys.length; i++) {
-                // 添加到数组尾部
-                if (childNode[i] == null) {
-                    childNode[i] = node;
-                    break;
-                }
-                // 添加到数组中间
-                if (childNode[i].entrys[0].key > node.entrys[0].key) {
-                    System.arraycopy(childNode, i, childNode, i + 1, M - i - 1);
-                    childNode[i] = node;
-                    break;
-                }
-            }
-        }
-
-
     }
+
 
     /**
      * 定位一个键值对，其实现了 Map.Entry<K, V> 接口
-     *
-     * @param <K>
-     * @param <V>
      */
-    private static class Entry<K, V> implements Map.Entry<K, V> {
+    private static class Entry implements Comparable<Entry> {
 
-        final K key;
-        V value;
+        final int key;
+        String value;
 
-        public Entry(K key, V value) {
+        public Entry(int key, String value) {
             this.key = key;
             this.value = value;
         }
 
-        @Override
-        public K getKey() {
+        public int getKey() {
             return key;
         }
 
-        @Override
-        public V getValue() {
+        public String getValue() {
             return value;
         }
 
-        @Override
-        public V setValue(V newValue) {
-            V oldValue = this.value;
+        public String setValue(String newValue) {
+            String oldValue = this.value;
             this.value = newValue;
             return oldValue;
+        }
+
+        /**
+         * 按照key进行排序
+         *
+         * @param o
+         * @return
+         */
+        @Override
+        public int compareTo(Entry o) {
+            return Integer.compare(key, o.getKey());
         }
     }
 }
