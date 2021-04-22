f01bcdb22042053f2da1de004f3b80d596bd5323
gozhuyinglong <gozhuyinglong@163.com>
2021-04-22 14:13:48 +0800

    Netty

---
+++ netty-demo/src/main/java/io/github/gozhuyinglong/netty/aio/AioClient.java
@@ -0,0 +1,45 @@
+package io.github.gozhuyinglong.netty.aio;
+
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.AsynchronousSocketChannel;
+import java.util.Scanner;
+
+/**
+ * @author ZhuYinglong
+ * @date 2021/4/22 0022
+ */
+public class AioClient {
+
+    private static final String HOST = "127.0.0.1";
+    private static final int PORT = 8080;
+
+    public static void main(String[] args) throws Exception {
+        // 打开一个异步的 Socket 通道
+        AsynchronousSocketChannel socketChannel = AsynchronousSocketChannel.open();
+        // 连接指定的服务端，并同步获取连接结果。get()操作会阻塞，直到连接成功。
+        socketChannel.connect(new InetSocketAddress(HOST, PORT)).get();
+        // 申请一个1024字节的缓冲区
+        ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
+
+        // 获取控制台输入内容，输入 quit 会退出输入，并关闭通道
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.print("请输入：");
+            String content = scanner.nextLine();
+            if ("quit".equals(content)) {
+                break;
+            }
+            // 将控制台输入的内容写入缓冲区
+            byteBuffer.put(content.getBytes());
+            // 反转缓冲区（从写入变为读取）
+            byteBuffer.flip();
+            // 将缓冲区中的内容写入到 Socket 通道中。get()操作会阻塞，直到写入成功。
+            socketChannel.write(byteBuffer).get();
+            // 清除缓冲区
+            byteBuffer.clear();
+        }
+        scanner.close();
+        socketChannel.close();
+    }
+}
--- netty-demo/src/main/java/io/github/gozhuyinglong/netty/aio/AioServer.java
+++ netty-demo/src/main/java/io/github/gozhuyinglong/netty/aio/AioServer.java
@@ -1,13 +1,12 @@
 package io.github.gozhuyinglong.netty.aio;
 
+import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
 import java.nio.channels.AsynchronousServerSocketChannel;
 import java.nio.channels.AsynchronousSocketChannel;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Future;
+import java.nio.channels.CompletionHandler;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
 
 /**
  * @author ZhuYinglong
@@ -17,45 +16,93 @@ public class AioServer {
 
     private static final int PORT = 8080;
 
-    public static void main(String[] args) throws Exception {
+    private AsynchronousServerSocketChannel serverSocketChannel;
 
-        // 打开一个 AsynchronousServerSocketChannel
-        AsynchronousServerSocketChannel serverSocketChannel = AsynchronousServerSocketChannel.open();
-        // 将此通道与本地端口进行绑定
-        serverSocketChannel.bind(new InetSocketAddress(PORT));
-        System.out.printf("[%s] - 服务端启动了，端口为：%s\n", Thread.currentThread().getName(), PORT);
+    public AioServer(int port) {
+        try {
+            // 打开一个异步的 ServerSocket 通道
+            serverSocketChannel = AsynchronousServerSocketChannel.open();
+            // 绑定本地端口
+            serverSocketChannel.bind(new InetSocketAddress(port));
+            System.out.printf("[%s] - 服务端启动了，端口为：%s\n", Thread.currentThread().getName(), port);
+            // 调用接收处理器
+            acceptHandler();
 
-        while (true) {
-            // 监听客户端连接（同步）
-            Future<AsynchronousSocketChannel> future = serverSocketChannel.accept();
-            // 获取客户端 Socket 通道
-            AsynchronousSocketChannel socketChannel = future.get();
-            System.out.printf("[%s] - 有一个客户端连上来了 - %s\n", Thread.currentThread().getName(), socketChannel.getRemoteAddress());
-            new Thread(() -> socketHandler(socketChannel)).start();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
+    }
+
+    /**
+     * 接收处理器
+     */
+    private void acceptHandler() {
+        // 接收一个客户端连接，若有客户端连接会自动通知 CompletionHandler 进行处理。注：该操作不会阻塞
+        serverSocketChannel.accept(this, new CompletionHandler<AsynchronousSocketChannel, AioServer>() {
+            @Override
+            public void completed(AsynchronousSocketChannel result, AioServer attachment) {
+                try {
+                    // 一个客户端连接后，继续接收下一个连接
+                    attachment.acceptHandler();
+                    System.out.printf("[%s] - 有一个客户端连上来了 - %s\n", Thread.currentThread().getName(), result.getRemoteAddress());
+                    // Socket处理器
+                    socketHandler(result);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void failed(Throwable exc, AioServer attachment) {
+                // 一个客户端连接后，无论成功或失败都要接收下一个连接
+                attachment.acceptHandler();
+                System.out.printf("[%s] - 客户连接失败 - %s\n", Thread.currentThread().getName(), exc.getMessage());
+            }
+        });
+    }
+
+    /**
+     * Socket 处理器
+     * @param socketChannel
+     */
+    private void socketHandler(AsynchronousSocketChannel socketChannel) {
+        // 申请一个1024个字节的缓冲区
+        ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
+        // 读取该通道的内容至缓冲区（将内容写入缓冲区），若有客户端写入内容，会自动通知到 CompletionHandler 进行处理。注：该操作不会阻塞
+        socketChannel.read(byteBuffer, byteBuffer, new CompletionHandler<Integer, ByteBuffer>() {
+            @Override
+            public void completed(Integer result, ByteBuffer attachment) {
+                // 继续读取该客户的消息
+                socketHandler(socketChannel);
+                // 将缓冲区进行反转（刚才是写入，反转后变为读取）
+                attachment.flip();
+                // 读取缓冲区中的内容，并转为字符串
+                String content = new String(attachment.array(), 0, attachment.limit());
+                System.out.printf("[%s] - 接收客户端发来的内容：%s\n", Thread.currentThread().getName(), content);
+                // 清除缓冲区
+                attachment.clear();
+            }
+
+            @Override
+            public void failed(Throwable exc, ByteBuffer attachment) {
+                System.out.printf("[%s] - 客户端断开连接！\n", Thread.currentThread().getName());
+                try {
+                    // 关闭当前 Socket 通道
+                    socketChannel.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
 
 
     }
 
-    private static void socketHandler(AsynchronousSocketChannel socketChannel) {
-        // 申请一个1024个字节的缓冲区
-        ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
-        while (true) {
-            // 读取该通道的内容至缓冲区（将内容写入缓冲区）
-            try {
-                if (socketChannel.read(byteBuffer).get() > 0) {
-                    // 将缓冲区进行反转（刚才是写入，反转后变为读取）
-                    byteBuffer.flip();
-                    // 读取缓冲区中的内容，并转为字符串
-                    String content = new String(byteBuffer.array(), 0, byteBuffer.limit());
-                    System.out.printf("[%s] - 接收客户端发来的内容：%s\n", Thread.currentThread().getName(), content);
-                    // 清除缓冲区
-                    byteBuffer.clear();
-                }
-            } catch (InterruptedException | ExecutionException e) {
-                e.printStackTrace();
-            }
-        }
+
+    public static void main(String[] args) throws Exception {
+
+        AioServer server = new AioServer(PORT);
+        TimeUnit.SECONDS.sleep(Integer.MAX_VALUE);
 
     }
 }
