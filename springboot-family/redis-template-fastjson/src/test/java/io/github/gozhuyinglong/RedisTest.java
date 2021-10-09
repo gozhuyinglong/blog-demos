1f61a048ecfd2d91c131aec82a9e00e5bda7f09c
gozhuyinglong <gozhuyinglong@163.com>
2021-10-10 00:26:58 +0800

    RedisTemplate支持FastJson

--- springboot-family/redis-template-fastjson/src/test/java/io/github/gozhuyinglong/RedisTest.java
+++ springboot-family/redis-template-fastjson/src/test/java/io/github/gozhuyinglong/RedisTest.java
@@ -1,5 +1,7 @@
 package io.github.gozhuyinglong;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import io.github.gozhuyinglong.pojo.Person;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,11 +19,20 @@ public class RedisTest {
     private RedisTemplate<String, Object> redisTemplate;
 
     @Test
-    public void test() {
+    public void testSet() {
         Person person = new Person();
         person.setId(1L);
         person.setName("码农StayUp");
         person.setAge(18);
         redisTemplate.opsForValue().set("person", person);
+    }
+
+    @Test
+    public void testGet() {
+        Object o = redisTemplate.opsForValue().get("person");
+        System.out.println(o);
+        // 转换为 Person 对象
+        Person person = JSON.parseObject((String) o, Person.class);
+        System.out.println(JSON.toJSONString(person));
     }
 }
